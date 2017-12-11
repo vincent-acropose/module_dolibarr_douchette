b4db4235479ec195db58464f6578bf66d0a8159e
Acropose <vincent.labarre@acropose.com>
2017-12-11 16:16:05 +0100

    Creation du module douchette

---
+++ lib/douchette.lib.php
@@ -0,0 +1,105 @@
+<?php
+/* <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2015 ATM Consulting <support@atm-consulting.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *	\file		lib/douchette.lib.php
+ *	\ingroup	douchette
+ *	\brief		This file is an example module library
+ *				Put some comments here
+ */
+
+function douchetteAdminPrepareHead()
+{
+    global $langs, $conf;
+
+    $langs->load("douchette@douchette");
+
+    $h = 0;
+    $head = array();
+
+    $head[$h][0] = dol_buildpath("/douchette/admin/douchette_setup.php", 1);
+    $head[$h][1] = $langs->trans("Parameters");
+    $head[$h][2] = 'settings';
+    $h++;
+    $head[$h][0] = dol_buildpath("/douchette/admin/douchette_about.php", 1);
+    $head[$h][1] = $langs->trans("About");
+    $head[$h][2] = 'about';
+    $h++;
+
+    // Show more tabs from modules
+    // Entries must be declared in modules descriptor with line
+    //$this->tabs = array(
+    //	'entity:+tabname:Title:@douchette:/douchette/mypage.php?id=__ID__'
+    //); // to add new tab
+    //$this->tabs = array(
+    //	'entity:-tabname:Title:@douchette:/douchette/mypage.php?id=__ID__'
+    //); // to remove a tab
+    complete_head_from_modules($conf, $langs, $object, $head, $h, 'douchette');
+
+    return $head;
+}
+
+/**
+ * Return array of tabs to used on pages for third parties cards.
+ *
+ * @param 	Tdouchette	$object		Object company shown
+ * @return 	array				Array of tabs
+ */
+function douchette_prepare_head(Tdouchette $object)
+{
+    global $db, $langs, $conf, $user;
+    $h = 0;
+    $head = array();
+    $head[$h][0] = dol_buildpath('/douchette/card.php', 1).'?id='.$object->getId();
+    $head[$h][1] = $langs->trans("douchetteCard");
+    $head[$h][2] = 'card';
+    $h++;
+	
+	// Show more tabs from modules
+    // Entries must be declared in modules descriptor with line
+    // $this->tabs = array('entity:+tabname:Title:@douchette:/douchette/mypage.php?id=__ID__');   to add new tab
+    // $this->tabs = array('entity:-tabname:Title:@douchette:/douchette/mypage.php?id=__ID__');   to remove a tab
+    complete_head_from_modules($conf,$langs,$object,$head,$h,'douchette');
+	
+	return $head;
+}
+
+function getFormConfirm(&$PDOdb, &$form, &$object, $action)
+{
+    global $langs,$conf,$user;
+
+    $formconfirm = '';
+
+    if ($action == 'validate' && !empty($user->rights->douchette->write))
+    {
+        $text = $langs->trans('ConfirmValidatedouchette', $object->ref);
+        $formconfirm = $form->formconfirm($_SERVER['PHP_SELF'] . '?id=' . $object->id, $langs->trans('Validatedouchette'), $text, 'confirm_validate', '', 0, 1);
+    }
+    elseif ($action == 'delete' && !empty($user->rights->douchette->write))
+    {
+        $text = $langs->trans('ConfirmDeletedouchette');
+        $formconfirm = $form->formconfirm($_SERVER['PHP_SELF'] . '?id=' . $object->id, $langs->trans('Deletedouchette'), $text, 'confirm_delete', '', 0, 1);
+    }
+    elseif ($action == 'clone' && !empty($user->rights->douchette->write))
+    {
+        $text = $langs->trans('ConfirmClonedouchette', $object->ref);
+        $formconfirm = $form->formconfirm($_SERVER['PHP_SELF'] . '?id=' . $object->id, $langs->trans('Clonedouchette'), $text, 'confirm_clone', '', 0, 1);
+    }
+
+    return $formconfirm;
+}
---
+++ script/create-maj-base.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Script créant et vérifiant que les champs requis s'ajoutent bien
+ */
+
+if(!defined('INC_FROM_DOLIBARR')) {
+	define('INC_FROM_CRON_SCRIPT', true);
+
+	require('../config.php');
+
+}
+
+
+/* uncomment
+
+
+dol_include_once('/douchette/class/douchette.class.php');
+
+$PDOdb=new TPDOdb;
+
+$o=new Tdouchette;
+$o->init_db_by_vars($PDOdb);
+
+$o=new TdouchetteChild;
+$o->init_db_by_vars($PDOdb);
+*/
